f844d92fb5579cf3d16ab716c1caa60c53c674bc
Ethan Haynes <eghaynes17@gmail.com>
2018-11-03 22:40:22 -0500

    adding default root path var

--- getDiskUsage.py
+++ getDiskUsage.py
@@ -1,6 +1,8 @@
 import sys, subprocess, os, socketserver
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
+    root_path = os.environ.get('ROOT_PATH') if os.environ.get('ROOT_PATH') else b''
+
     def handle(self):
         data = self.request.recv(1024).strip().split(b'\n')
          
@@ -8,22 +10,22 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             method, path, protocol = data.pop(0).split(b' ')
             if method == b'GET':
                 if os.path.exists(path):
-                    disk_usage = subprocess.run(["du", "-x", path], capture_output=True)
-                    disk_usage = disk_usage.stdout.splitlines()
+                    process_output = subprocess.run(['du', '-x', self.root_path + path], capture_output=True)
+                    disk_usage = process_output.stdout.splitlines()
                     out = {}
                     for file in disk_usage:
                         f_bytes, f_name = file.split('\t'.encode())
-                        out[f_name.decode("utf-8")] = int(f_bytes)
+                        out[f_name.decode('utf-8')] = int(f_bytes)
                     print(out)
                     print(method, path, protocol)
         
         self.request.sendall(str(data).encode())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     HOST = os.environ.get('HOST')
     if not HOST: 
-        HOST = "localhost"
+        HOST = 'localhost'
 
     PORT = os.environ.get('PORT')
     if not PORT: 
